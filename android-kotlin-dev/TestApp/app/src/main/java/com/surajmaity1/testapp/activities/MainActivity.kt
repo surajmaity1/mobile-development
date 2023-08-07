bb60e219256cf318e5c8e7f4f25ca7bbaba53257
surajmaity1 <surajmaity981@gmail.com>
2023-08-07 17:27:58 +0530

    feat: Explore and Refine Added

--- android-kotlin-dev/TestApp/app/src/main/java/com/surajmaity1/testapp/activities/MainActivity.kt
+++ android-kotlin-dev/TestApp/app/src/main/java/com/surajmaity1/testapp/activities/MainActivity.kt
@@ -3,6 +3,10 @@ package com.surajmaity1.testapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.surajmaity1.testapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val intent = Intent(this@MainActivity, ExploreActivity::class.java)
-        val intent = Intent(this@MainActivity, RefineActivity::class.java)
-        startActivity(intent)
-        finish()
+        setUpActionBar()
+
+        val refine = findViewById<Button>(R.id.refine_screen_activity)
+        val explore = findViewById<Button>(R.id.explore_screen_activity)
+
+        refine.setOnClickListener {
+            val intent = Intent(this@MainActivity, RefineActivity::class.java)
+            startActivity(intent)
+        }
+
+        explore.setOnClickListener {
+            val intent = Intent(this@MainActivity, ExploreActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setUpActionBar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_main_activity)
+        val mainTitleToolbar = findViewById<TextView>(R.id.main_title_toolbar_main)
+
+
+        setSupportActionBar(toolbar)
+
+        mainTitleToolbar.text = resources.getText(R.string.main_activity)
     }
 
 
